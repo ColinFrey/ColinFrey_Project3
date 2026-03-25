89b28a18893bda67663ab647abfb774939465d0c
ColinFrey <Acefrey2020@gmail.com>
2026-03-25 08:29:24 -0500

    work on cursor tracking

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-// JS scripts placed here
+const fish = document.getElementById('character');
+
+let mouseX = 0;
+let mouseY = 0;
+let fishX = 0;
+let fishY = 0;
+
+const ease = 0.06; 
+
+window.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+
+  let dx = mouseX - fishX;
+  let dy = mouseY - fishY;
+
+  fishX += dx * ease;
+  fishY += dy * ease;
+
+
+  fish.style.transform = `translate(${fishX - 75}px, ${fishY - 75}px)`;
+  
+  requestAnimationFrame(animate);
+}
+
+fish.onerror = () => {
+    console.error("The browser cannot find the image. Check if 'img/PufferfishSlim.jpeg' exists.");
+};
+
+animate();
